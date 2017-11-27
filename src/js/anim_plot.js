bebcfb14cb531c21ff8d2a3cae5238ea1c2daea0
Johannes Harmse <johannesharmse91@gmail.com>
2017-11-27 09:53:28 -0800

    Exploration of animation

---
+++ src/js/anim_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#anim_plot').scianimator({
+	    'images': ['anim_dir/anim_plot1.png', 'anim_dir/anim_plot2.png', 'anim_dir/anim_plot3.png', 'anim_dir/anim_plot4.png', 'anim_dir/anim_plot5.png', 'anim_dir/anim_plot6.png', 'anim_dir/anim_plot7.png', 'anim_dir/anim_plot8.png', 'anim_dir/anim_plot9.png', 'anim_dir/anim_plot10.png', 'anim_dir/anim_plot11.png', 'anim_dir/anim_plot12.png', 'anim_dir/anim_plot13.png', 'anim_dir/anim_plot14.png', 'anim_dir/anim_plot15.png', 'anim_dir/anim_plot16.png', 'anim_dir/anim_plot17.png', 'anim_dir/anim_plot18.png', 'anim_dir/anim_plot19.png', 'anim_dir/anim_plot20.png', 'anim_dir/anim_plot21.png', 'anim_dir/anim_plot22.png', 'anim_dir/anim_plot23.png', 'anim_dir/anim_plot24.png', 'anim_dir/anim_plot25.png', 'anim_dir/anim_plot26.png', 'anim_dir/anim_plot27.png', 'anim_dir/anim_plot28.png', 'anim_dir/anim_plot29.png', 'anim_dir/anim_plot30.png', 'anim_dir/anim_plot31.png', 'anim_dir/anim_plot32.png', 'anim_dir/anim_plot33.png', 'anim_dir/anim_plot34.png', 'anim_dir/anim_plot35.png', 'anim_dir/anim_plot36.png', 'anim_dir/anim_plot37.png', 'anim_dir/anim_plot38.png', 'anim_dir/anim_plot39.png', 'anim_dir/anim_plot40.png', 'anim_dir/anim_plot41.png', 'anim_dir/anim_plot42.png', 'anim_dir/anim_plot43.png', 'anim_dir/anim_plot44.png', 'anim_dir/anim_plot45.png', 'anim_dir/anim_plot46.png', 'anim_dir/anim_plot47.png', 'anim_dir/anim_plot48.png', 'anim_dir/anim_plot49.png', 'anim_dir/anim_plot50.png', 'anim_dir/anim_plot51.png', 'anim_dir/anim_plot52.png', 'anim_dir/anim_plot53.png', 'anim_dir/anim_plot54.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#anim_plot').scianimator('play');
+    });
+})(jQuery);
