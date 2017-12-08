a9257fc51b5296e0d43399e2f5bdeab80d0727d1
Johannes Harmse <johannesharmse91@gmail.com>
2017-12-07 17:37:40 -0800

    Write function for plotting daily, weekly or annual locations

--- src/data_visualization.R
+++ src/data_visualization.R
@@ -10,3 +10,92 @@ library(animation)
 
 browse_summary <- readRDS(file = "../data/R_temp/browse_summary.rds")
 location_summary <- readRDS(file = "../data/R_temp/location_summary.rds")
+
+# Load base map from Google
+
+
+
+plot_map <- function(location_summary, period = 10, plot_period = "daily"){
+  
+  # location_summary <- location_summary
+  
+  mapData <- get_googlemap(center = c(0, 0), zoom = 1, maptype = "hybrid", source = "google", size = c(360, 240), scale = 1)
+  
+  ggmapdata <- ggmap(mapData)
+  
+  temp_filt <- location_summary
+  
+  start = 1
+  start_plot = 1
+  lat_start = 0
+  long_start = 0
+  
+  plot_seq <- seq(period, nrow(temp_filt), 1)
+  
+  temp = data_frame()
+  
+  
+  
+  if (plot_period == "daily"){
+    for (count in 1:nrow(temp_filt)){
+      if(interval(temp_filt$time[start], temp_filt$time[count])/ddays(1) >= 1){
+        
+        searches <- paste(browse_summary$title)
+        
+        p <- ggmapdata + 
+          geom_point(data = temp_filt[1:count, ], aes(x = long, 
+                                                      y = lat), 
+                     alpha = 0.05, fill = "light blue", size = 2, 
+                     colour = "blue") + 
+          geom_point(data = temp_filt[count, ], aes(x = long, 
+                                                    y = lat), 
+                     alpha = 0.5, fill = "red", size = 3, colour = "red")
+        
+        print(p)
+        
+        start = count
+        
+      }
+    }
+  }else if (plot_period == "weekly"){
+    for (count in 1:nrow(temp_filt)){
+      if(interval(temp_filt$time[start], temp_filt$time[count])/dweeks(1) >= 1){
+        
+        p <- ggmapdata + 
+          geom_point(data = temp_filt[1:count, ], aes(x = long, 
+                                                      y = lat), 
+                     alpha = 0.05, fill = "light blue", size = 2, 
+                     colour = "blue") + 
+          geom_point(data = temp_filt[count, ], aes(x = long, 
+                                                    y = lat), 
+                     alpha = 0.5, fill = "red", size = 3, colour = "red")
+        
+        print(p)
+        
+        start = count
+        
+      }
+    }
+  }else if (plot_period == "annually"){
+    for (count in 1:nrow(temp_filt)){
+      if(interval(temp_filt$time[start], temp_filt$time[count])/dyears(1) >= 1){
+        
+        p <- ggmapdata + 
+          geom_point(data = temp_filt[1:count, ], aes(x = long, 
+                                                      y = lat), 
+                     alpha = 0.05, fill = "light blue", size = 2, 
+                     colour = "blue") + 
+          geom_point(data = temp_filt[count, ], aes(x = long, 
+                                                    y = lat), 
+                     alpha = 0.5, fill = "red", size = 3, colour = "red")
+        
+        print(p)
+        
+        start = count
+        
+      }
+    }
+  }
+  
+}
+
