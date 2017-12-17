98b222b4f3c2bcc7e769b55f71f44713494f3019
Johannes Harmse <johannesharmse91@gmail.com>
2017-12-16 17:01:15 -0800

    Test restoring libraries via first called script

--- src/data_import.R
+++ src/data_import.R
@@ -19,6 +19,8 @@
 #
 # The script writes the data sources as R objects to a location where the next script will call it from.
 
+#restore libraries
+packrat::restore()
 
 library(rjson)
 library(stringr)
