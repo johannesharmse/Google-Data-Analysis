0c0e1d4dbbea7526274e32acefa427f40c9536ba
Johannes Harmse <johannesharmse91@gmail.com>
2017-12-17 00:30:44 -0800

    Remove installr package

--- src/report_render.R
+++ src/report_render.R
@@ -6,11 +6,5 @@
 
 # The purpose of this code is to automate the rendering process of the Rmd report to knitted formats.
 
-# installing/loading the package:
-if(!require(installr)) { install.packages("installr"); require(installr)} #load / install+load installr
-
-# Installing pandoc
-install.pandoc(use_regex = FALSE)
-
 library('rmarkdown')
 rmarkdown::render("results/Google_Data_Analysis_Report.Rmd")
