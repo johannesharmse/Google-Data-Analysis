40d6d8f0d3ff77f682c88e85ddbf2dc57ba3e3ed
Johannes Harmse <johannesharmse91@gmail.com>
2017-12-16 19:21:35 -0800

    Add pandoc dependency

--- src/report_render.R
+++ src/report_render.R
@@ -6,5 +6,11 @@
 
 # The purpose of this code is to automate the rendering process of the Rmd report to knitted formats.
 
+# installing/loading the package:
+if(!require(installr)) { install.packages("installr"); require(installr)} #load / install+load installr
+
+# Installing pandoc
+install.pandoc(use_regex = FALSE)
+
 library('rmarkdown')
 rmarkdown::render("results/Google_Data_Analysis_Report.Rmd")
